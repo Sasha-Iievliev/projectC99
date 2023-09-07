61cf09803440617e894abf70322351b7adec4f42
Sasha-Iievliev <121599132+Sasha-Iievliev@users.noreply.github.com>
2023-09-07 21:30:21 +1000

    Add files via upload

---
+++ event.js
@@ -0,0 +1,15 @@
+function birth(){
+    username = document.getElementById('username').value;
+    localStorage.setItem('username', username);
+    window.location = 'birth.html';
+}
+function hallowing(){
+    username = document.getElementById('username').value;
+    localStorage.setItem('username', username);
+    window.location = 'hallowing.html';
+}
+function easter(){
+    username = document.getElementById('username').value;
+    localStorage.setItem('username', username);
+    window.location = 'easter.html';
+}
